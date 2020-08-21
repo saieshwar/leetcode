b5e81e60276ad5db010e9a3b4fb5b247b3b75d18
Sai Eshwar Reddy Bobbili <bobbilisaieshwarreddy@gmail.com>
2020-08-20 21:01:37 -0400

    0 ms

--- solutions/287. Find the Duplicate Number.java
+++ solutions/287. Find the Duplicate Number.java
@@ -8,7 +8,7 @@ class Solution {
         while(tortoise!=rabbit){
             tortoise = nums[tortoise];
             rabbit = nums[nums[rabbit]];
-        }
+        }
         
         int apointer = nums[0];
         int bpointer = tortoise; // or rabbit anything is same pointed
@@ -17,8 +17,26 @@ class Solution {
             bpointer = nums[bpointer];
         }
         return bpointer;
-            
         
-       
+        
+        
+//         //Another approach by knowledge center
+//         int maxidx = 0;
+//         int curr_max = 0;
+//         int n = nums.length;
+//         for(int i = 0; i < nums.length;i++){
+//             int id = nums[i]%n;//going tp tje indewx firectly by moduling by length
+//             nums[id]+=n; //adding length to that index based element
+//         }
+//         //now find max element 
+//         for(int i = 0 ; i < n;i++){
+//             if(nums[i]>curr_max){
+//                 curr_max = nums[i];
+//                 maxidx = i;
+//             }
+//             nums[i]%=5;
+//         }
+        
+//         return maxidx;
     }
 }
