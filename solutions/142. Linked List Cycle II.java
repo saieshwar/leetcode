9356844a0fd71950b3b973d255df5c2b31a28a7e
Sai Eshwar Reddy Bobbili <bobbilisaieshwarreddy@gmail.com>
2020-08-23 23:21:11 -0400

    1 ms

--- solutions/142. Linked List Cycle II.java
+++ solutions/142. Linked List Cycle II.java
@@ -11,24 +11,43 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        try{
-        do{ 
-            slow= slow.next;
-            fast = fast.next.next;
-        }while(fast!=slow);  
-        slow = head;
-        while(slow!=fast){
-            slow = slow.next;
-            fast = fast.next;
+        // ListNode slow = head;
+        // ListNode fast = head;
+//         try{
+//         do{ 
+//             slow= slow.next;
+//             fast = fast.next.next;
+//         }while(fast!=slow);  
+//         slow = head;
+//         while(slow!=fast){
+//             slow = slow.next;
+//             fast = fast.next;
+//         }
+// }
+// catch(NullPointerException e) {
+//             return null;
+// }
+        
+//         return fast;
+        
+//     }
+        //another approach
+        ListNode hare = head;
+        ListNode tortoise = head;
+        ListNode start =head;
+        while(hare!=null && hare.next!=null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if(tortoise==hare) {
+                 // there is a cycle, find the start of the loop
+                while(start != tortoise) {
+                    start = start.next;
+                    tortoise = tortoise.next;
+                }
+                return start;
+            }
         }
+        return null;
 }
-catch(NullPointerException e) {
-            return null;
 }
-        
-        return fast;
-        
-    }
-}
+​
