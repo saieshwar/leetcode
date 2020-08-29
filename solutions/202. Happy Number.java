b6be977c23b5ad39a323dcd3c87d6a3e12c5edc9
Sai Eshwar Reddy Bobbili <bobbilisaieshwarreddy@gmail.com>
2020-08-29 15:10:01 -0400

    1 ms

--- solutions/202. Happy Number.java
+++ solutions/202. Happy Number.java
@@ -1,18 +1,33 @@
-class Solution {
-    public boolean isHappy(int n) {
-        int sum =0;
-        while(n>0){
-            sum = sum + (int)Math.pow(n%10,2);
-            n= n/10;
-            }
-        if(sum >=10)
-            return isHappy(sum);
-        else{ 
-            if(sum==1||sum==7)
-                return true;
-            else
-                return false;
-        }
+//                     return true;
+//                 n =sum;
+//             }
+//         return false;
+//         }
         
+//     }
+        
+        //****************Using floyd detetction algorithm***************
+        int slow =n,fast = n;
+        do{
+        slow = sumofsquares(slow);
+        fast = sumofsquares(fast);
+        fast = sumofsquares(fast);
+            if(fast ==1)
+                return true;
+        }while(slow!=fast);
+        
+        return false;
     }
+​
+        
+            
+        private int sumofsquares(int n){
+            int sum = 0;
+            while(n>0){
+                sum = sum + (int)Math.pow(n%10,2);
+                n= n/10;
+                }
+            return sum;
+        }
 }
+​
